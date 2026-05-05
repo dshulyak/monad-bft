8a16b64976215537318a7928fae6ccc74f9cac4f
Dmitry Shulyak <yashulyak@gmail.com>
2026-05-05 09:41:27 -0400

    statesync: migrate executor metrics
    Change-Id: I344610e1b1601749eca4f385b6d7ed34afe464a2

--- monad-statesync/src/lib.rs
+++ monad-statesync/src/lib.rs
@@ -73,6 +73,18 @@ monad_executor::metric_consts! {
     }
 }
 
+fn init_executor_metrics() -> ExecutorMetrics {
+    ExecutorMetrics::with_metric_defs([
+        GAUGE_STATESYNC_SYNCING,
+        GAUGE_STATESYNC_PROGRESS_ESTIMATE,
+        GAUGE_STATESYNC_LAST_TARGET,
+        GAUGE_STATESYNC_SERVER_PENDING_REQUESTS,
+        GAUGE_STATESYNC_SERVER_NUM_SYNCDONE_SUCCESS,
+        GAUGE_STATESYNC_SERVER_NUM_SYNCDONE_FAILED,
+        GAUGE_STATESYNC_SERVER_TOTAL_SERVICE_TIME_US,
+    ])
+}
+
 pub struct StateSync<ST, SCT>
 where
     ST: CertificateSignatureRecoverable,
@@ -115,7 +127,7 @@ where
             )),
 
             waker: None,
-            metrics: Default::default(),
+            metrics: init_executor_metrics(),
             _phantom: Default::default(),
         };
 
@@ -125,10 +137,12 @@ where
     }
 
     fn update_syncing_metrics(&mut self) {
-        self.metrics[GAUGE_STATESYNC_SYNCING] = match &self.mode {
-            StateSyncMode::Sync(_) => 1,
-            StateSyncMode::Live(_) => 0,
-        };
+        self.metrics
+            .gauge(GAUGE_STATESYNC_SYNCING)
+            .set(match &self.mode {
+                StateSyncMode::Sync(_) => 1,
+                StateSyncMode::Live(_) => 0,
+            });
     }
 }
 
@@ -156,7 +170,9 @@ where
                             unreachable!("Live -> Sync is not a valid state transition")
                         }
                     };
-                    self.metrics[GAUGE_STATESYNC_LAST_TARGET] = header.0.number;
+                    self.metrics
+                        .gauge(GAUGE_STATESYNC_LAST_TARGET)
+                        .set(header.0.number);
                     statesync.update_target(header.0);
                     if let Some(waker) = self.waker.take() {
                         waker.wake();
@@ -301,7 +317,9 @@ where
         match &mut this.mode {
             StateSyncMode::Sync(sync) => {
                 if let Some(progress) = sync.progress_estimate() {
-                    this.metrics[GAUGE_STATESYNC_PROGRESS_ESTIMATE] = progress.0;
+                    this.metrics
+                        .gauge(GAUGE_STATESYNC_PROGRESS_ESTIMATE)
+                        .set(progress.0);
                 }
 
                 if let Poll::Ready(event) = sync.poll_next_unpin(cx) {
@@ -330,15 +348,21 @@ where
                 }
             }
             StateSyncMode::Live(execution_ipc) => {
-                this.metrics[GAUGE_STATESYNC_SERVER_PENDING_REQUESTS] =
-                    execution_ipc.pending_request_len() as u64
-                        + execution_ipc.is_servicing_request() as u64;
-                this.metrics[GAUGE_STATESYNC_SERVER_NUM_SYNCDONE_SUCCESS] =
-                    execution_ipc.num_syncdone_success() as u64;
-                this.metrics[GAUGE_STATESYNC_SERVER_NUM_SYNCDONE_FAILED] =
-                    execution_ipc.num_syncdone_failed() as u64;
-                this.metrics[GAUGE_STATESYNC_SERVER_TOTAL_SERVICE_TIME_US] =
-                    execution_ipc.total_service_time_us() as u64;
+                this.metrics
+                    .gauge(GAUGE_STATESYNC_SERVER_PENDING_REQUESTS)
+                    .set(
+                        execution_ipc.pending_request_len() as u64
+                            + execution_ipc.is_servicing_request() as u64,
+                    );
+                this.metrics
+                    .gauge(GAUGE_STATESYNC_SERVER_NUM_SYNCDONE_SUCCESS)
+                    .set(execution_ipc.num_syncdone_success() as u64);
+                this.metrics
+                    .gauge(GAUGE_STATESYNC_SERVER_NUM_SYNCDONE_FAILED)
+                    .set(execution_ipc.num_syncdone_failed() as u64);
+                this.metrics
+                    .gauge(GAUGE_STATESYNC_SERVER_TOTAL_SERVICE_TIME_US)
+                    .set(execution_ipc.total_service_time_us() as u64);
                 if let Poll::Ready(maybe_response) = execution_ipc.response_rx.poll_recv(cx) {
                     let (to, message, completion) = maybe_response.expect("did StateSyncIpc die?");
                     tracing::debug!(
